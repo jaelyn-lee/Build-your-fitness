7dd8e11c73883d540c0e1b5def843558e76fbbeb
Jaelyn Lee <leejay1020@gmail.com>
2023-07-25 02:00:17 +1200

    create getExercisesByDifficulty DB function

--- server/db/db.ts
+++ server/db/db.ts
@@ -5,6 +5,11 @@ export function getExercises(db = connection) {
   return db('exercises').select()
 }
 
+//GET EXERCISES BY DIFFICULTY
+export function getExercisesByDifficulty(difficulty: number, db = connection) {
+  return db('exercises').select().where('difficulty', difficulty)
+}
+
 //ADD NEW USER INFORMATION
 export function addUsers(db = connection) {
   return db('users').insert()
