1dbe3bf9b47dba5f2b9edd9f6101a03b3ffae541
Jaelyn Lee <leejay1020@gmail.com>
2023-08-10 09:58:15 +1200

    workout routine page styling done

--- client/components/WorkoutRoutine.tsx
+++ client/components/WorkoutRoutine.tsx
@@ -30,8 +30,6 @@ export default function WorkoutRoutine() {
     fetchData()
   }, [])
 
-  console.log(fitnessLevel)
-
   const filteredExercises = fetchedExercises.filter(
     (exercise) =>
       (fitnessLevel >= 0 &&
@@ -45,23 +43,53 @@ export default function WorkoutRoutine() {
         exercise.difficulty === 'Advanced'),
   )
 
+  //Split the filtered workout routine into 4 different days
+  const exercisesPerDay = 4
+
+  const workoutDays = []
+  for (let i = 0; i < 4; i++) {
+    const startIndex = i * exercisesPerDay
+    const endIndex = startIndex + exercisesPerDay
+    workoutDays.push(filteredExercises.slice(startIndex, endIndex))
+  }
+
   return (
     <>
-      <h1>Thank you, {user.first_name}! Here is your workout routine!</h1>
+      <div className="flex flex-col items-center justify-center text-4xl font-extrabold mb-10">
+        <h1>Thank you, {user.first_name}!</h1>
+        <h1>Here is your 4-day workout routine!</h1>
+      </div>
       <div className="flex justify-center items-center">
-        <ul className="flex">
-          {filteredExercises.map((routine, index) => (
-            <div
-              className="border-2 border-white border-solid p-10 m-2 bg-white text-black w-44"
-              key={index}
-            >
-              <li>Name: {routine.name}</li>
-              <li>Description: {routine.description}</li>
-              <li>Target Muscle: {routine.muscle_group}</li>
-              <li>Reps per set: {routine.reps}</li>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4">
+          {workoutDays.map((exercises, dayIndex) => (
+            <div key={dayIndex}>
+              <ul className="list-disc pl-6 border-2 border-white border-solid p-4 bg-white text-black list-none">
+                <h2 className="text-3xl font-semibold text-red text-center mb-3">
+                  Day {dayIndex + 1}
+                </h2>
+                {exercises.map((routine, exerciseIndex) => (
+                  <li key={exerciseIndex} className="text-center">
+                    <div className="mb-3">
+                      <p>
+                        <span className="font-extrabold text-xl">
+                          {routine.name}{' '}
+                        </span>{' '}
+                        ({routine.muscle_group})
+                      </p>
+                      <p>
+                        <span className="font-bold text-xl">3</span> sets x{' '}
+                        <span className="font-bold text-xl">
+                          {routine.reps}
+                        </span>{' '}
+                        reps
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           ))}
-        </ul>
+        </div>
       </div>
     </>
   )
